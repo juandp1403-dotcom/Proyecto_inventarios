2cc8f4cee89cea2139db5e670f0a3c124c831d22
silvanamateus2008-ui <silvanamateus2008@gmail.com>
2026-05-13 15:29:52 -0500

    sssss

--- run.py
+++ run.py
@@ -8,6 +8,5 @@ app = create_app()
 
 if __name__ == '__main__':
     init_database(app)
-
-    debug_mode = os.getenv('FLASK_ENV') == 'development'
-    app.run(host='0.0.0.0', port=5000, debug=debug_mode)
+    debug_mode = os.getenv('FLASK_ENV') == 'true'
+    app.run(host='0.0.0.0', port=5000, debug="true")
