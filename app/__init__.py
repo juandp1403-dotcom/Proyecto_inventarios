696e9764c37825bd12477b7796c8debc1f4405fc
JuanDP1403 <juandp1403@gmail.com>
2026-05-15 14:30:23 -0500

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, session
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy()
 
@@ -100,39 +99,3 @@ def create_app():
         return render_template('dashboard.html', current_role=get_user_role())
 
     return app
-
-
-def init_database():
-    app = create_app()
-    with app.app_context():
-        from app.models.usuario import Usuario
-        from app.models.rol import Rol
-        from app.models.categoria import Categoria
-        
-        db.create_all()
-        
-        for rol_name in ['admin', 'auditor', 'revisor', 'instructor', 'aprendiz']:
-            if not Rol.query.filter_by(nombre=rol_name).first():
-                db.session.add(Rol(nombre=rol_name))
-        
-        if not Categoria.query.filter_by(nombre='General').first():
-            db.session.add(Categoria(nombre='General'))
-        
-        db.session.commit()
-        
-        rol_admin = Rol.query.filter_by(nombre='admin').first()
-        
-        if not Usuario.query.filter_by(email='admin@gmail.com').first():
-            admin = Usuario(
-                nombre='admin',
-                email='admin@gmail.com',
-                password=generate_password_hash('123456'),
-                id_rol=rol_admin.id,
-                aprobado=True,
-                activo=True
-            )
-            db.session.add(admin)
-            db.session.commit()
-            print("Admin creado: admin@gmail.com / 123456")
-        
-        print("Base de datos inicializada - Ambiente 208 eliminado")
